4801478a0453a08a78f4b36213a67ae0d0895b85
mes32 <stockman.mike@gmail.com>
2024-06-27 21:37:06 -0500

    play sounds in response to key presses

--- 01-drum-kit/script.js
+++ 01-drum-kit/script.js
@@ -1,32 +1,50 @@
+const clap = new Audio('sounds/clap.wav')
+const hihat = new Audio('sounds/hihat.wav')
+const kick = new Audio('sounds/kick.wav')
+const openhat = new Audio('sounds/openhat.wav')
+const boom = new Audio('sounds/boom.wav')
+const ride = new Audio('sounds/ride.wav')
+const snare = new Audio('sounds/snare.wav')
+const tom = new Audio('sounds/tom.wav')
+const tink = new Audio('sounds/tink.wav')
 
 document.addEventListener('keydown', (event) => {
   switch (event.key.toLowerCase()) {
     case 'a':
       document.getElementById('a-clap').classList.add('active')
+      clap.play()
       break
     case 's':
       document.getElementById('s-hihat').classList.add('active')
+      hihat.play()
       break
     case 'd':
       document.getElementById('d-kick').classList.add('active')
+      kick.play()
       break
     case 'f':
       document.getElementById('f-openhat').classList.add('active')
+      openhat.play()
       break
     case 'g':
       document.getElementById('g-boom').classList.add('active')
+      boom.play()
       break
     case 'h':
       document.getElementById('h-ride').classList.add('active')
+      ride.play()
       break
     case 'j':
-      document.getElementById('j-spare').classList.add('active')
+      document.getElementById('j-snare').classList.add('active')
+      snare.play()
       break
     case 'k':
       document.getElementById('k-tom').classList.add('active')
+      tom.play()
       break
     case 'l':
-      document.getElementById('l-link').classList.add('active')
+      document.getElementById('l-tink').classList.add('active')
+      tink.play()
       break
     default:
       break
@@ -54,13 +72,13 @@ document.addEventListener('keyup', (event) => {
       document.getElementById('h-ride').classList.remove('active')
       break
     case 'j':
-      document.getElementById('j-spare').classList.remove('active')
+      document.getElementById('j-snare').classList.remove('active')
       break
     case 'k':
       document.getElementById('k-tom').classList.remove('active')
       break
     case 'l':
-      document.getElementById('l-link').classList.remove('active')
+      document.getElementById('l-tink').classList.remove('active')
       break
     default:
       break
