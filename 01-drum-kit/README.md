2f3262e47914979c4c59e6bd0c8fed2c88bb7e38
mes32 <stockman.mike@gmail.com>
2024-06-28 23:55:56 -0500

    refactor switch statement to use a map object instead

--- 01-drum-kit/README.md
+++ 01-drum-kit/README.md
@@ -1,10 +1,5 @@
 # 01 - JavaScript Drum Kit
 
-## Todo
-
-- Refactor event handlers
-- Add equivalent mouse event handlers matching the behavior of the keyboard handlers
-
 ## Notes
 
 - Custom `data-key` element attribute allows not using a large switch statement to map from key presses to associated audio clips
@@ -12,3 +7,4 @@
 - CSS `transition: all` and CSS shorthand properties
 - Need to reset the current time on audio clips to allow playing them in close succession 
 - It is possible to add an event listener on 'transitioned' to check specifically for elements that have changed
+- Replacing switch with a map lookup in JavaScript https://medium.com/front-end-weekly/switch-case-if-else-or-a-lookup-map-a-study-case-de1c801d944
--- 01-drum-kit/script.js
+++ 01-drum-kit/script.js
@@ -1,95 +1,33 @@
-const clap = new Audio('sounds/clap.wav')
-const hihat = new Audio('sounds/hihat.wav')
-const kick = new Audio('sounds/kick.wav')
-const openhat = new Audio('sounds/openhat.wav')
-const boom = new Audio('sounds/boom.wav')
-const ride = new Audio('sounds/ride.wav')
-const snare = new Audio('sounds/snare.wav')
-const tom = new Audio('sounds/tom.wav')
-const tink = new Audio('sounds/tink.wav')
+const clap = new Audio('sounds/clap.wav');
+const hihat = new Audio('sounds/hihat.wav');
+const kick = new Audio('sounds/kick.wav');
+const openhat = new Audio('sounds/openhat.wav');
+const boom = new Audio('sounds/boom.wav');
+const ride = new Audio('sounds/ride.wav');
+const snare = new Audio('sounds/snare.wav');
+const tom = new Audio('sounds/tom.wav');
+const tink = new Audio('sounds/tink.wav');
+
+const mapFromEventKey = {
+  'a': { elementId: 'clap', audio: clap },
+  's': { elementId: 'hihat', audio: hihat },
+  'd': { elementId: 'kick', audio: kick },
+  'f': { elementId: 'openhat', audio: openhat },
+  'g': { elementId: 'boom', audio: boom },
+  'h': { elementId: 'ride', audio: ride },
+  'j': { elementId: 'snare', audio: snare },
+  'k': { elementId: 'tom', audio: tom },
+  'l': { elementId: 'tink', audio: tink },
+}
 
 document.addEventListener('keydown', (event) => {
-  switch (event.key.toLowerCase()) {
-    case 'a':
-      document.getElementById('a-clap').classList.add('active')
-      clap.currentTime = 0
-      clap.play()
-      break
-    case 's':
-      document.getElementById('s-hihat').classList.add('active')
-      hihat.currentTime = 0
-      hihat.play()
-      break
-    case 'd':
-      document.getElementById('d-kick').classList.add('active')
-      kick.currentTime = 0
-      kick.play()
-      break
-    case 'f':
-      document.getElementById('f-openhat').classList.add('active')
-      openhat.currentTime = 0
-      openhat.play()
-      break
-    case 'g':
-      document.getElementById('g-boom').classList.add('active')
-      boom.currentTime = 0
-      boom.play()
-      break
-    case 'h':
-      document.getElementById('h-ride').classList.add('active')
-      ride.currentTime = 0
-      ride.play()
-      break
-    case 'j':
-      document.getElementById('j-snare').classList.add('active')
-      snare.currentTime = 0
-      snare.play()
-      break
-    case 'k':
-      document.getElementById('k-tom').classList.add('active')
-      tom.currentTime = 0
-      tom.play()
-      break
-    case 'l':
-      document.getElementById('l-tink').classList.add('active')
-      tink.currentTime = 0
-      tink.play()
-      break
-    default:
-      break
-  }
+  const m = mapFromEventKey[event.key.toLowerCase()];
+  document.getElementById(m.elementId).classList.add('active');
+  m.audio.currentTime = 0;
+  m.audio.play();
 })
 
 document.addEventListener('keyup', (event) => {
-  switch (event.key.toLowerCase()) {
-    case 'a':
-      document.getElementById('a-clap').classList.remove('active')
-      break
-    case 's':
-      document.getElementById('s-hihat').classList.remove('active')
-      break
-    case 'd':
-      document.getElementById('d-kick').classList.remove('active')
-      break
-    case 'f':
-      document.getElementById('f-openhat').classList.remove('active')
-      break
-    case 'g':
-      document.getElementById('g-boom').classList.remove('active')
-      break
-    case 'h':
-      document.getElementById('h-ride').classList.remove('active')
-      break
-    case 'j':
-      document.getElementById('j-snare').classList.remove('active')
-      break
-    case 'k':
-      document.getElementById('k-tom').classList.remove('active')
-      break
-    case 'l':
-      document.getElementById('l-tink').classList.remove('active')
-      break
-    default:
-      break
-  }
+  const m = mapFromEventKey[event.key.toLowerCase()];
+  document.getElementById(m.elementId).classList.remove('active');
 })
