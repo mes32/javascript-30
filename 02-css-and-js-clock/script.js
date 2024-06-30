604fbc3b2ab558b40d7562de4ab373207c52caae
mes32 <stockman.mike@gmail.com>
2024-06-30 00:28:51 -0500

    get the clock ticking

--- 02-css-and-js-clock/script.js
+++ 02-css-and-js-clock/script.js
@@ -1,9 +1,38 @@
-function rotateHandElement(handElementId, degrees) {
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');
+const digitalSpan = document.getElementById('digital-span');
+
+function runUpdateLoop() {
+  var now = new Date();
+  update(now);
+
+  setInterval(() => {
+    now = new Date();
+    update(now);
+  }, 200);
+}
+
+runUpdateLoop();
+
+function update(now) {
+  digitalSpan.textContent = `${now.toLocaleTimeString()}`;
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  rotateHandElement(secondHand, 360.0 * seconds / 60);
+  rotateHandElement(minuteHand, 360.0 * (60 * minutes + seconds) / 3600);
+  rotateHandElement(hourHand, 360.0 * (60 * hours + minutes) / 3600);
+}
+
+function rotateHandElement(handElement, degrees) {
   const radians = degrees * Math.PI / 180;
   const x = 100 * Math.sin(radians);
   const y = 100 * (1 - Math.cos(radians));
 
-  const handElement = document.getElementById(handElementId);
+  // const handElement = document.getElementById(handElementId);
   handElement.style.transform = `translate(${x}px,${y}px)`;
   handElement.style.transform += `rotate(${degrees}deg)`;
 }
