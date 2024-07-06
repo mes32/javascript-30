bd3ad8d62ac9596cb709ce3f245530b7225c0565
mes32 <stockman.mike@gmail.com>
2024-07-06 14:26:29 -0500

    fix bug where non-panel elements could get the '.active' class

--- 05-flex-panel-gallery/script.js
+++ 05-flex-panel-gallery/script.js
@@ -1,8 +1,10 @@
 const panels = document.querySelectorAll(".panel");
 
+console.log(panels);
+
 panels.forEach((panel) => {
   panel.addEventListener("click", (event) => {
     panels.forEach((p) => p.classList.remove("active"));
-    event.target.classList.add("active");
+    event.target.closest(".panel").classList.add("active");
   });
 });
